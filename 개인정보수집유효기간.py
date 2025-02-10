40e7721483a60f9ad38e04a05fd88cd34485bfee
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-02-10 16:34:41 +0900

    Update 개인정보수집유효기간.py

--- 개인정보수집유효기간.py
+++ 개인정보수집유효기간.py
@@ -13,25 +13,25 @@ A= 6 mon/ B= 12 mon/ C= 3mon
 """
 
 
-def get_day(ymd):
-    y, m, d = map(int, ymd.split('.'))
-    return (y * 12 * 28) + (m * 28) + d  # 날짜를 일 단위로 변환
+def timestamp(day):
+    t = day.split(".")
+    return (int(t[0]) * 12 * 28) + (int(t[1]) * 28) + int(t[2])
+
 
 def solution(today, terms, privacies):
     answer = []
-    today_days = get_day(today) 
-    
-    term_dict = {}
-    for term in terms:
-        t_type, t_months = term.split()
-        term_dict[t_type] = int(t_months) * 28  # 기간을 일 단위로 변환
+    terms_map = {}
 
-    for i, privacy in enumerate(privacies):
-        date, p_type = privacy.split()
-        expiry_date = get_day(date) + term_dict[p_type]  # 만료일 계산
-        
-        if expiry_date <= today_days:  # 오늘 날짜와 비교
+    for i in terms:
+        s = i.split(" ")
+        terms_map[s[0]] = int(s[1]) * 28 - 1
+
+    t = times(today)
+    for i in range(len(privacies)):
+        s = privacies[i].split(" ")
+        d = times(s[0]) + terms_map[s[1]]
+        # print(d, t)
+        if d < t:
             answer.append(i + 1)
 
-    return answer
-
+    return answe
