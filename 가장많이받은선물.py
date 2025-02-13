3e85f9dd746f79fd5266331319baa10ddf9ca918
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-02-13 14:40:03 +0900

    Update 가장많이받은선물.py

--- 가장많이받은선물.py
+++ 가장많이받은선물.py
@@ -38,3 +38,41 @@ def solution(friends, gifts):
 
     # 4. 가장 많이 받는 사람 찾기
     return max(receive_count) /*다음달에 가장많이 받을 선물 개수 반환*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        -----------------------------------------------
+
+def solution(friends, gifts):
+    f = {v: i for i, v in enumerate(friends)}
+    l = len(friends)
+    p = [0] * l
+    answer = [0] * l
+    gr = [[0] * l for i in range(l)]
+    for i in gifts:
+        a, b = i.split()
+        gr[f[a]][f[b]] += 1
+    for i in range(l):
+        p[i] = sum(gr[i]) - sum([k[i] for k in gr])
+
+    for i in range(l):
+        for j in range(l):
+            if gr[i][j] > gr[j][i]:
+                answer[i] += 1
+            elif gr[i][j] == gr[j][i]:
+                if p[i] > p[j]:
+                    answer[i] += 1
+    return max(answer)
+
