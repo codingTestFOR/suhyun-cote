68c3e37e84e2e17f7f02cd6b030c56f86a0325bd
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-03-16 21:11:43 +0900

    Update 수묶기.py

--- 백준/수묶기.py
+++ 백준/수묶기.py
@@ -9,3 +9,22 @@
 
 
 
+def max_sum_of_sequence(N, sequence):
+    sequence.sort(reverse=True)
+    result = 0
+    i = 0
+    
+    # 2 이상인 숫자들은 묶어서 곱하고, 1과 0은 각각 더해주기
+    while i < N - 1:
+        if sequence[i] > 1 and sequence[i + 1] > 1:
+            result += sequence[i] * sequence[i + 1]
+            i += 2
+        else:
+            result += sequence[i]
+            i += 1
+
+    # 마지막 남은 수 처리 (1이거나 0이면 그냥 더함)
+    if i < N:
+        result += sequence[i]
+
+    return result
