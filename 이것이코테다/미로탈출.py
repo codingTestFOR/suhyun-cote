bda2afe58a8515ae32f87607155203af78a8e066
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-04-14 16:33:47 +0900

    Create 미로탈출.py

---
+++ 이것이코테다/미로탈출.py
@@ -0,0 +1,49 @@
+#미로탈출 
+"""
+N × M 크기의 미로에 갇혀 있다.
+미로는 여러 개의 칸(격자)으로 구성되어 있고,
+각 칸은 1(길) 또는 0(벽)로 표시되어 있다.
+
+당신은 (1, 1)에서 시작해서 (N, M) 위치까지 이동해야 함.
+이동할 때는 상, 하, 좌, 우로 한 칸씩 이동 가능함.
+
+구현 방법: BFS 너비우선탐색
+
+input 
+5 6 ( 최소이동칸의 개수)
+1101010
+1111
+"""
+
+n,m= map(int,input().split())
+road =[list(map(int, input())) for i in range(n)]
+
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+def escape(x,y):
+    queue = [(x,y)]
+    
+    while queue:
+        x,y=queue[0] 
+        queue=queue[1:] 
+
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+
+            if nx<0 or ny<0 or nx>= n or ny>= m:
+                continue
+
+            
+            if road[nx][ny]== 0:
+                continue
+              
+            if road[nx][ny] == 1: #첫번재 방문시 기록
+                road[nx][ny] = road[x][y] +1
+                queue.append((nx, ny))
+
+    return road[n-1][m-1]
+
+print(escape(0, 0))
